1b61d79783f39390b3f83236dd452a759cf5496e
Naomi Lagat <naomiejerutoe@gmail.com>
2024-04-23 18:28:42 +0300

    run migrations

---
+++ server/migrations/versions/c692894db30a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c692894db30a
+Revises: c692894db30a
+Create Date: 2024-04-23 18:26:07.703462
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c692894db30a'
+down_revision = 'c692894db30a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
